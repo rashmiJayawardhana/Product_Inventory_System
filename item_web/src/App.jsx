c116d7ef1e6c11ba33607329a679b4d4e559f30f
Rashmi Jayawardhana <155569390+rashmiJayawardhana@users.noreply.github.com>
2025-02-14 01:01:24 +0530

    Table

--- item_web/src/App.jsx
+++ item_web/src/App.jsx
@@ -1,7 +1,35 @@
 import { useState } from 'react'
 import './App.css'
+import {useTable} from 'react-table'
+import * as React from 'react'
+import axios from 'axios'
 
 function App() {
+  const [items,setItems]=useState([])
+  const columns=React.useMemo(()=>[
+    {Header:"ItemID", accessor:"item_id"},
+    {Header:"Name", accessor:"name"},
+    {Header:"Description", accessor:"description"},
+    {Header:"Price", accessor:"price"}
+  ],[]);
+
+  const data=React.useMemo(()=>items,[]);
+  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow}=useTable({columns, data:items});
+
+  const getItems = () => {
+    axios.get("http://127.0.0.1:8000/api/items/")
+      .then((response) => {
+        console.log("Data received:", response.data);
+        setItems(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching items:", error);
+      });
+  };
+ 
+  React.useEffect(()=>{
+    getItems();
+  },[])
 
   return (
     <>
@@ -20,10 +48,38 @@ function App() {
             <label htmlFor='price'>Price</label> <br/>
             <input className='addeditinput' type='text' name='price' id='price'/>
           </div>
-          <button>Add</button>
+          <button className='addButton'>Add</button>
+          <button className='cancelButton'>Cancel</button>
 
         </div>
         <input className='searchinput' type="search" name="inputsearch" id="inputsearch" placeholder="Search Items Here"/>
+
+        <table className='item-table' {...getTableProps()}>
+          <thead>
+            {...headerGroups.map((hdg)=>(
+              <tr {...hdg.getHeaderGroupProps()} key={hdg.id}>
+                {hdg.headers.map((column)=>(
+                  <th {...column.getHeaderProps()} key={column.id}>{column.render('Header')}</th>
+                ))}           
+              </tr>
+            ))}
+            <tr>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row)=>{
+              prepareRow(row);
+              return(<tr {...row.getRowProps()} key={row.id}>
+                {row.cells.map((cell)=>(
+                  <td {...cell.getCellProps()} key={cell.id}>
+                    {cell.render('Cell')}
+                  </td>
+                ))}            
+            </tr>)
+            })}            
+          </tbody>
+        </table>
       </div>
     </>
   )
