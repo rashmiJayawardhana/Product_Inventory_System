6afb688b1b77813dc1b9b7cd1783c1d7cddea27e
Rashmi Jayawardhana <155569390+rashmiJayawardhana@users.noreply.github.com>
2025-02-13 23:58:38 +0530

    Frontend Form

--- item_web/src/App.jsx
+++ item_web/src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='item-main'>
+        <h3>Product Inventory System</h3>
+        <div className='addeditpanel'>
+          <div className='addeditpaneldiv'>
+            <label htmlFor='name'>Name</label> <br/>
+            <input className='addeditinput' type='text' name='name' id='name'/>
+          </div>
+          <div className='addeditpaneldiv'>
+            <label htmlFor='description'>Description</label> <br/>
+            <input className='addeditinput' type='text' name='description' id='description'/>
+          </div>
+          <div className='addeditpaneldiv'>
+            <label htmlFor='price'>Price</label> <br/>
+            <input className='addeditinput' type='text' name='price' id='price'/>
+          </div>
+          <button>Add</button>
+
+        </div>
+        <input className='searchinput' type="search" name="inputsearch" id="inputsearch" placeholder="Search Items Here"/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
